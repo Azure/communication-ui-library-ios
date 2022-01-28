4d446d13ce5842433164160dd3f41b2f6bee08aa
Kevin Guo <v-kevinguo@microsoft.com>
2022-01-27 17:48:01 -0800

    start to use outlined style for anonymous user, accent otherwise

--- AzureCommunicationUI/AzureCommunicationUI/Presentation/FluentUI/Wrapper/CompositeParticipantsListCell.swift
+++ AzureCommunicationUI/AzureCommunicationUI/Presentation/FluentUI/Wrapper/CompositeParticipantsListCell.swift
@@ -7,12 +7,19 @@ import Foundation
 import FluentUI
 
 class CompositeParticipantsListCell: TableViewCell {
+
+    /// Set up the participant list item  in the participant list
+    /// - Parameters:
+    ///   - displayName: The display name shown on the participant list cell
+    ///   - isMuted: tells whether this participant is muted
+    ///   - partipantHasEmptyName: tells whether this participant had an empty name when joined the call
+    ///   - accessoryType: accessory type
     func setup(displayName: String,
                isMuted: Bool,
                partipantHasEmptyName: Bool,
                accessoryType: TableViewCellAccessoryType = .none) {
         let isNameEmpty = displayName.trimmingCharacters(in: .whitespaces).isEmpty
-        let avatar = MSFAvatar(style: .accent, size: .medium)
+        let avatar = MSFAvatar(style: partipantHasEmptyName ? .outlined : .accent, size: .medium)
 
         avatar.state.primaryText = !partipantHasEmptyName ? displayName : nil
         let avatarView = avatar.view
@@ -28,13 +35,15 @@ class CompositeParticipantsListCell: TableViewCell {
             ? StyleProvider.color.popoverColor
             : StyleProvider.color.drawerColor
 
-        if isNameEmpty || partipantHasEmptyName {
+        let isAnonymousUser = isNameEmpty || partipantHasEmptyName
+
+        if isAnonymousUser {
             setTitleLabelTextColor(color: UIColor.compositeColor(CompositeColor.mute))
         } else {
             setTitleLabelTextColor(color: UIColor.compositeColor(CompositeColor.onSurface))
         }
 
-        setup(title: isNameEmpty ? StringConstants.defaultEmptyName : displayName,
+        setup(title: isAnonymousUser ? StringConstants.defaultEmptyName : displayName,
               customView: avatarView,
               customAccessoryView: micImageView)
     }
