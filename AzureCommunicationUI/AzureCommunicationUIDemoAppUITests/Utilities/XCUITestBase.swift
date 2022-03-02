844b029f666a3b3ba7acb4c335dc6bc4a1d767f5
AmyL219 <longamy@microsoft.com>
2022-03-02 14:03:17 -0800

    Clean up XCUITestBase class

--- AzureCommunicationUI/AzureCommunicationUIDemoAppUITests/Utilities/XCUITestBase.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoAppUITests/Utilities/XCUITestBase.swift
@@ -7,21 +7,4 @@ import XCTest
 
 class XCUITestBase: XCTestCase {
 
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-
-        continueAfterFailure = false
-        XCUIApplication().launch()
-    }
-
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
