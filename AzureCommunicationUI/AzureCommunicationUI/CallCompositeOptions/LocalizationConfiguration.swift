5f28d123cbd7253997b4433b2d56edb0b2bbf463
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2022-03-04 12:35:27 -0800

    improve function documentation

--- AzureCommunicationUI/AzureCommunicationUI/CallCompositeOptions/LocalizationConfiguration.swift
+++ AzureCommunicationUI/AzureCommunicationUI/CallCompositeOptions/LocalizationConfiguration.swift
@@ -12,7 +12,7 @@ public struct LocalizationConfiguration {
     let isRightToLeft: Bool
 
     /// Create an instance of LocalizationConfiguration to customize localization.
-    /// - Parameter locale: Name of the language locale.
+    /// - Parameter locale: String representing the locale code (ie. en,fr,  zh-Hant, zh-Hans, ...).
     /// - Parameter localizableFilename:Name of the localizable strings file to override
     ///  predefined Call Composite's localization string or to provide localization for an unsupported
     ///  language.  The key of the string should be matched with the one in AzureCommunicationUI.
@@ -29,7 +29,7 @@ public struct LocalizationConfiguration {
     }
 
     /// Create an instance of LocalizationConfiguration to customize localization.
-    /// - Parameter locale: Name of the language locale.
+    /// - Parameter locale: String representing the locale code (ie. en,fr,  zh-Hant, zh-Hans, ...).
     /// - Parameter customStrings: A dictionary of key-value pairs to override override
     ///  predefined Call Composite's localization string. The key of the string should be matched
     ///  with the one in AzureCommunicationUI.
@@ -45,13 +45,13 @@ public struct LocalizationConfiguration {
     }
 
     /// Get supported languages the Call Composite is localized to.
-    /// - Returns: A list of language names for the locales that has predefined localized strings for
+    /// - Returns: A list of language names for the locale codes that has predefined localized strings for
     public static func getSupportedLanguages() -> [String] {
         return LocalizationProvider.supportedLocales
     }
 
     /// Get the locale string dictionary for key-value pairs of a locale that is supported by Call Composite.
-    /// - Parameter locale: The name of the locale that is supported by Call Composite.
+    /// - Parameter locale: String representing locale code that is supported by Call Composite.
     /// - Returns: A dictionary of key-value pair for localization keys with the string value of a given locale.
     ///  Returns empty dictionary if the locale is not supported.
     public static func getLocaleStringDict(locale: String) -> [String: String] {
