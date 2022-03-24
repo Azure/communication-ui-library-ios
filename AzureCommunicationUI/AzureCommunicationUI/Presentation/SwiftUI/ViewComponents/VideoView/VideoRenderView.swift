6d7538a071a2eead88cfbc44331b30da81f23749
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2022-03-24 14:27:11 -0700

    Updated bridging RendererView into SwiftUI

--- AzureCommunicationUI/AzureCommunicationUI/Presentation/SwiftUI/ViewComponents/VideoView/VideoRenderView.swift
+++ AzureCommunicationUI/AzureCommunicationUI/Presentation/SwiftUI/ViewComponents/VideoView/VideoRenderView.swift
@@ -10,11 +10,41 @@ import Combine
 struct VideoRendererView: UIViewRepresentable {
     let rendererView: UIView
 
-    func makeUIView(context: Context) -> UIView {
-        return rendererView
+    func makeUIView(context: Context) -> VideoRendererUIView {
+        return VideoRendererUIView(rendererView: rendererView)
     }
 
-    func updateUIView(_ uiView: UIView, context: Context) {
+    func updateUIView(_ uiView: VideoRendererUIView, context: Context) {
+        uiView.update(rendererView: rendererView)
+    }
+}
 
+class VideoRendererUIView: UIView {
+    private var rendererView: UIView?
+
+    init(rendererView: UIView) {
+        super.init(frame: .zero)
+        update(rendererView: rendererView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func update(rendererView: UIView) {
+        guard self.rendererView != rendererView
+        else { return }
+
+        rendererView.removeFromSuperview()
+        for view in subviews {
+            view.removeFromSuperview()
+        }
+        addSubview(rendererView)
+        rendererView.translatesAutoresizingMaskIntoConstraints = false
+        rendererView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        rendererView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        rendererView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        rendererView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        self.rendererView = rendererView
     }
 }
