f780dd93694df2f84ffc1164bdf376fc18fabe0c
Josh Lai <joshlai@microsoft.com>
2022-03-25 15:53:59 -0700

    [AuthenticationHelper] Demo app was never calling completion handler in case of error.

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/AuthenticationHelper.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/AuthenticationHelper.swift
@@ -9,7 +9,13 @@ import AzureCommunicationCalling
 class AuthenticationHelper {
     static func getCommunicationToken(tokenUrl: URL) -> TokenRefresher {
         struct TokenResponse: Decodable {
+            let userAndToken: UserAndTokenResponse
+            let endpointUrl: String?
+        }
+        struct UserAndTokenResponse: Decodable {
             let token: String
+            let expiresOn: String?
+            let user: String?
         }
         return { completionHandler in
             var urlRequest = URLRequest(url: tokenUrl, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
@@ -17,13 +23,15 @@ class AuthenticationHelper {
             URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
                 if let error = error {
                     print(error)
+                    completionHandler(nil, error)
                 } else if let data = data {
                     do {
                         let res = try JSONDecoder().decode(TokenResponse.self, from: data)
-                        print(res.token)
-                        completionHandler(res.token, nil)
+                        print(res.userAndToken.token)
+                        completionHandler(res.userAndToken.token, nil)
                     } catch let error {
                         print(error)
+                        completionHandler(nil, error)
                     }
                 }
             }.resume()
