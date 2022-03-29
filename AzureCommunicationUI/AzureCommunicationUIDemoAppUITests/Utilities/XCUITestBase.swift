10b7eb7c3af98dad4498bdb4c2ba5869a6c6c85b
Kevin Guo <v-kevinguo@microsoft.com>
2022-03-29 14:16:42 -0700

    Refactored XCUITests methods in child class into the base XCUITest class. Renamed the UXUITest child class

--- AzureCommunicationUI/AzureCommunicationUIDemoAppUITests/Utilities/XCUITestBase.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoAppUITests/Utilities/XCUITestBase.swift
@@ -7,4 +7,65 @@ import XCTest
 
 class XCUITestBase: XCTestCase {
 
+    enum CompositeSampleInterface {
+        case swiftUI
+        case uiKit
+
+        var name: String {
+            switch self {
+            case .swiftUI:
+                return "Swift UI"
+            case .uiKit:
+                return "UI Kit"
+            }
+        }
+    }
+
+    enum CompositeMeetingType {
+        case groupCall
+        case teamsCall
+
+        var name: String {
+            switch self {
+            case .groupCall:
+                return "Group Call"
+            case .teamsCall:
+                return "Teams Meeting"
+            }
+        }
+    }
+
+    var app: XCUIApplication?
+
+}
+
+extension XCUITestBase {
+
+    func tapEnabledButton(buttonName: String, shouldWait: Bool) {
+        guard let button = app?.buttons[buttonName] else {
+            return
+        }
+        if shouldWait {
+            waitEnabled(for: button)
+        }
+        button.tap()
+    }
+
+    func tapButton(buttonName: String, shouldWait: Bool) {
+        guard let button = app?.buttons[buttonName] else {
+            return
+        }
+        if shouldWait {
+            wait(for: button)
+        }
+        button.tap()
+    }
+
+    func tapInterfaceFor(_ interface: CompositeSampleInterface) {
+        app?.buttons[interface.name].tap()
+    }
+
+    func tapMeetingType(_ meetingType: CompositeMeetingType) {
+        app?.buttons[meetingType.name].tap()
+    }
 }
