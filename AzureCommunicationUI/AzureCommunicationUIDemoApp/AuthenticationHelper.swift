cc6c6b18d69cae4c4999c41d4d731cc7a2b11ca9
Josh Lai <joshlai@microsoft.com>
2022-03-30 15:45:26 -0700

    [AuthenticationHelper] Revert the decodable struct

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/AuthenticationHelper.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/AuthenticationHelper.swift
@@ -9,16 +9,7 @@ import AzureCommunicationCalling
 class AuthenticationHelper {
     static func getCommunicationToken(tokenUrl: URL) -> TokenRefresher {
         struct TokenResponse: Decodable {
-            let userAndToken: UserAndTokenResponse
-            let endpointUrl: String?
-        }
-        struct UserAndTokenResponse: Decodable {
             let token: String
-            let expiresOn: String?
-            let user: CommunicationUserIdResponse?
-        }
-        struct CommunicationUserIdResponse: Decodable {
-            let communicationUserId: String?
         }
         return { completionHandler in
             var urlRequest = URLRequest(url: tokenUrl, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
@@ -30,8 +21,8 @@ class AuthenticationHelper {
                 } else if let data = data {
                     do {
                         let res = try JSONDecoder().decode(TokenResponse.self, from: data)
-                        print(res.userAndToken.token)
-                        completionHandler(res.userAndToken.token, nil)
+                        print(res.token)
+                        completionHandler(res.token, nil)
                     } catch let error {
                         print(error)
                         completionHandler(nil, error)
