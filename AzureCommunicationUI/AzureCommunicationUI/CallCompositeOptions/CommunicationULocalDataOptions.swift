a3feb3bf79d103541886c407acdf39495b9e92ad
Josh Lai <joshlai@microsoft.com>
2022-04-05 13:14:15 -0700

    [AvatarManager] Introduce the new avatar manager and unit tests

---
+++ AzureCommunicationUI/AzureCommunicationUI/CallCompositeOptions/CommunicationULocalDataOptions.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+import UIKit
+
+public struct CommunicationULocalDataOptions {
+   let localPersona: CommunicationUPersonaData
+
+}
+
+public struct CommunicationUPersonaData {
+    let avatar: UIImage?
+    let displaName: String?
+}
---
+++ AzureCommunicationUI/AzureCommunicationUI/Presentation/Manager/AvatarManager.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+import UIKit
+import AzureCommunicationCommon
+
+public class AvatarManager {
+    private let localKey: String = "local"
+
+    private let store: Store<AppState>
+    private var avatarCache = MappedSequence<String, Data>()
+
+    init(store: Store<AppState>) {
+        self.store = store
+    }
+
+    func setLocalAvatar(_ image: UIImage) {
+        if let rawData = image.pngData() {
+            avatarCache.append(forKey: localKey, value: rawData)
+        }
+    }
+
+    func getLocalAvatar() -> UIImage? {
+        if let data = avatarCache.value(forKey: localKey) {
+            return UIImage(data: data)
+        }
+
+        return nil
+    }
+}
