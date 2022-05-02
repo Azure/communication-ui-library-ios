78a93796e8c0b17edf96224024342efad0ea6509
AmyL219 <longamy@microsoft.com>
2022-05-02 08:54:39 -0700

    Add ACSCallEndReason extension

---
+++ AzureCommunicationUI/AzureCommunicationUI/Service/Calling/Extension/ACSCallEndReasonExtension.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+import AzureCommunicationCalling
+
+extension CallEndReason {
+    func toCompositeErrorCodeString(_ isCallConnected: Bool) -> String {
+        let callEndErrorCode = self.code
+        let callEndErrorSubCode = self.subcode
+
+        var compositeErrorCodeString = ""
+        switch callEndErrorCode {
+        case 0 :
+            if (callEndErrorSubCode == 5300 || callEndErrorSubCode == 5000),
+               isCallConnected {
+                compositeErrorCodeString = CallCompositeErrorCode.callEvicted
+            }
+        case 401:
+            compositeErrorCodeString = CallCompositeErrorCode.tokenExpired
+        case 487:
+            // Call cancelled by user as a happy path
+            break
+        default:
+            // For all other errorCodes:
+            // https://docs.microsoft.com/en-us/azure/communication-services/concepts/troubleshooting-info
+            compositeErrorCodeString = isCallConnected ? CallCompositeErrorCode.callEnd
+            : CallCompositeErrorCode.callJoin
+        }
+
+        return compositeErrorCodeString
+    }
+}
