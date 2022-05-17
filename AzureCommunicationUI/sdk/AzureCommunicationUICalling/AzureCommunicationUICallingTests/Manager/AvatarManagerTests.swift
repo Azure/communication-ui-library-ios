10b760e9d051f0743ae20790c090a2122b5ada21
Josh Lai <joshlai@microsoft.com>
2022-05-17 11:33:53 -0700

    [AvatarManagerTests] no longer load image but create a uiimage based on uicolor

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICallingTests/Manager/AvatarManagerTests.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICallingTests/Manager/AvatarManagerTests.swift
@@ -15,9 +15,7 @@ class AvatarManagerTests: XCTestCase {
     }
 
     func test_avatarManager_when_setLocalAvatar_then_getLocalAvatar_returnsSameUIImage() {
-        guard let mockImage = UIImage(named: "Icon/ic_fluent_call_end_24_filled",
-                                      in: Bundle(for: CallComposite.self),
-                                      compatibleWith: nil) else {
+        guard let mockImage = UIImage.make(withColor: .red) else {
             XCTFail("UIImage does not exist")
             return
         }
@@ -34,5 +32,20 @@ class AvatarManagerTests: XCTestCase {
         return CompositeAvatarViewManager(store: mockStoreFactory.store,
                                           localSettings: mockLocalSettings)
 
+    }
+}
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img
     }
 }
