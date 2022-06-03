a868f2596131067b231e6e69b55c82cc48dcf5f5
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2022-06-03 09:15:13 -0700

    [Refactor] Update set events functions (#227)

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICallingTests/DI/DependencyContainerTests.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICallingTests/DI/DependencyContainerTests.swift
@@ -27,7 +27,7 @@ class DependencyContainerTests: XCTestCase {
                                                   displayName: displayName)
         let participantViewData = ParticipantViewData(avatar: nil, displayName: nil)
         let localOptions = LocalOptions(participantViewData)
-        let callCompositeEventsHandler = CallCompositeEventsHandler()
+        let callCompositeEventsHandler = CallComposite.Events()
 
         dependencyContainer.registerDependencies(callConfiguration,
                                                  localOptions: localOptions,
