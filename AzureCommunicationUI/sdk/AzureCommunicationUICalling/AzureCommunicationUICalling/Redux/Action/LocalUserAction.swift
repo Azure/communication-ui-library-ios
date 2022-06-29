69cfa5772fb8edfa5bad2722c591d39aef430480
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-06-29 12:59:58 -0700

    Use exhaustive equality check

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICalling/Redux/Action/LocalUserAction.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICalling/Redux/Action/LocalUserAction.swift
@@ -40,10 +40,45 @@ enum LocalUserAction: Equatable {
     case audioDeviceChangeFailed(error: Error)
 
     static func == (lhs: LocalUserAction, rhs: LocalUserAction) -> Bool {
-        return lhs.typeString == rhs.typeString
-    }
 
-    private var typeString: String {
-        String(describing: self)
+        switch (lhs, rhs) {
+        case let (.cameraOnFailed(lErr), .cameraOnFailed(rErr)),
+            let (.cameraOffFailed(lErr), .cameraOffFailed(rErr)),
+            let (.cameraPausedFailed(lErr), .cameraPausedFailed(rErr)),
+            let (.cameraSwitchFailed(lErr), .cameraSwitchFailed(rErr)),
+            let (.microphoneOnFailed(lErr), .microphoneOnFailed(rErr)),
+            let (.microphoneOffFailed(lErr), .microphoneOffFailed(rErr)),
+            let (.audioDeviceChangeFailed(lErr), .audioDeviceChangeFailed(rErr)):
+
+            return (lErr as NSError).code == (rErr as NSError).code
+
+        case (.cameraPreviewOnTriggered, .cameraPreviewOnTriggered),
+            (.cameraOnTriggered, .cameraOnTriggered),
+            ( .cameraOffTriggered, .cameraOffTriggered),
+            ( .cameraOffSucceeded, .cameraOffSucceeded),
+            (.cameraPausedSucceeded, .cameraPausedSucceeded),
+            (.cameraSwitchTriggered, .cameraSwitchTriggered),
+            (.microphoneOnTriggered, .microphoneOnTriggered),
+            (.microphoneOffTriggered, .microphoneOffTriggered),
+            (.microphonePreviewOn, .microphonePreviewOn),
+            (.microphonePreviewOff, .microphonePreviewOff):
+            return true
+            
+        case let (.audioDeviceChangeRequested(lDev), .audioDeviceChangeRequested(rDev)),
+            let (.audioDeviceChangeSucceeded(lDev), .audioDeviceChangeSucceeded(rDev)):
+            return lDev == rDev
+
+        case let (.microphoneMuteStateUpdated(lMuted), .microphoneMuteStateUpdated(rMuted)):
+            return lMuted == rMuted
+
+        case let (.cameraOnSucceeded(lId), .cameraOnSucceeded(rId)):
+            return lId == rId
+
+        case let (.cameraSwitchSucceeded(lDev), .cameraSwitchSucceeded(rDev)):
+            return lDev == rDev
+
+        default:
+            return false
+        }
     }
 }
