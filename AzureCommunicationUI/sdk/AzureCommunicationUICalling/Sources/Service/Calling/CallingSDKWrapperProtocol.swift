e91f8f45099045418124bcbc4e337ba1bb2ae5b4
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-08-09 14:20:04 -0700

    Remove combine interface from calling SDK wrapper

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Service/Calling/CallingSDKWrapperProtocol.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Service/Calling/CallingSDKWrapperProtocol.swift
@@ -14,28 +14,17 @@ enum CameraDevice {
 
 protocol CallingSDKWrapperProtocol {
     func getRemoteParticipant(_ identifier: String) -> RemoteParticipant?
-    func startPreviewVideoStream() -> AnyPublisher<String, Error>
     func getLocalVideoStream(_ identifier: String) -> LocalVideoStream?
-    func setupCall() -> AnyPublisher<Void, Error>
-    func startCall(isCameraPreferred: Bool, isAudioPreferred: Bool) -> AnyPublisher<Void, Error>
-    func endCall() -> AnyPublisher<Void, Error>
-    func startCallLocalVideoStream() -> AnyPublisher<String, Error>
-    func stopLocalVideoStream() -> AnyPublisher<Void, Error>
-    func switchCamera() -> AnyPublisher<CameraDevice, Error>
-    func muteLocalMic() -> AnyPublisher<Void, Error>
-    func unmuteLocalMic() -> AnyPublisher<Void, Error>
-    func holdCall() -> AnyPublisher<Void, Error>
-    func resumeCall() -> AnyPublisher<Void, Error>
 
     func startPreviewVideoStream() async throws -> String
     func setupCall() async throws
-    func startCall() async throws
+    func startCall(isCameraPreferred: Bool, isAudioPreferred: Bool) async throws
     func endCall() async throws
     func startCallLocalVideoStream() async throws -> String
     func stopLocalVideoStream() async throws
     func switchCamera() async throws -> CameraDevice
     func muteLocalMic() async throws
-    func unumuteLocalMic() async throws
+    func unmuteLocalMic() async throws
     func holdCall() async throws
     func resumeCall() async throws
 
