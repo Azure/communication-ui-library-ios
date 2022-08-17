59df0be7d51d0ecc29435f59025314b051dc42d5
John <109105353+jpeng-ms@users.noreply.github.com>
2022-08-17 13:21:01 -0700

    Update ColorThemeProvider.swift

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Style/ColorThemeProvider.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Style/ColorThemeProvider.swift
@@ -15,6 +15,7 @@ class ColorThemeProvider {
     let primaryColorTint20: UIColor
     let primaryColorTint30: UIColor
 
+    // MARK: To Be Deleted After Refactoring Part 2 is done
     let backgroundColor = UIColor.compositeColor(.background)
     let gridLayoutBackground = UIColor.compositeColor(.gridLayoutBackground)
     let onSurfaceColor = UIColor.compositeColor(.onSurface)
@@ -41,6 +42,32 @@ class ColorThemeProvider {
     let textSecondary = UIColor.compositeColor(.textSecondary)
     let iconSecondary = UIColor.compositeColor(.iconSecondary)
 
+    // MARK: - Fluent UI Refactoring Starts - Part 1
+    // MARK: - Shared
+    let genericLabel: UIColor = FluentUI.Colors.textPrimary
+    let genericIcon: UIColor = FluentUI.Colors.textPrimary
+    let genericIconDisabled: UIColor = FluentUI.Colors.iconDisabled
+
+    // MARK: - Setup View Colours
+    lazy var setupTitleText: UIColor = genericLabel
+    lazy var videoBackground: UIColor = {
+        return dynamicColor(light: FluentUI.Colors.surfaceQuaternary,
+                            dark: FluentUI.Colors.surfaceTertiary)
+    }()
+    lazy var premissionIcon: UIColor = genericIcon
+    lazy var permissionText: UIColor = genericLabel
+    lazy var previewGradient = UIColor.black.withAlphaComponent(0.7)
+    lazy var warningLabel: UIColor = {
+        return dynamicColor(light: FluentUI.Colors.Palette.warningShade30.color,
+                            dark: UIColor.black)
+    }()
+    lazy var warningBanner: UIColor = {
+        return dynamicColor(light: FluentUI.Colors.Palette.warningTint40.color,
+                            dark: FluentUI.Colors.warning)
+    }()
+
+    // MARK: - Calling View Colours - Coming up in Part 2
+
     init(themeOptions: ThemeOptions?) {
         self.colorSchemeOverride = themeOptions?.colorSchemeOverride ?? .unspecified
 
@@ -48,6 +75,10 @@ class ColorThemeProvider {
         self.primaryColorTint10 = themeOptions?.primaryColorTint10 ?? Colors.Palette.communicationBlueTint10.color
         self.primaryColorTint20 = themeOptions?.primaryColorTint20 ?? Colors.Palette.communicationBlueTint20.color
         self.primaryColorTint30 = themeOptions?.primaryColorTint30 ?? Colors.Palette.communicationBlueTint30.color
+    }
+
+    private func dynamicColor(light: UIColor, dark: UIColor) -> UIColor {
+        return UIColor { $0.userInterfaceStyle == .dark ? dark : light }
     }
 }
 
