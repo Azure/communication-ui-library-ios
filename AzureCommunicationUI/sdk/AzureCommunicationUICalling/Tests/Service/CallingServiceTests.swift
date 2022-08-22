41277fe17a467e1aea0866cc593d9a40eb46ea7a
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-08-22 09:04:07 -0700

    [Refactor] Use async functions in the Calling SDK Components (#312)

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Service/CallingServiceTests.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Service/CallingServiceTests.swift
@@ -31,85 +31,85 @@ class CallingServiceTests: XCTestCase {
         callingService = nil
     }
 
-    func test_callingService_setupCall_shouldCallcallingSDKWrapperSetupCall() {
-        _ = callingService.setupCall()
+    func test_callingService_setupCall_shouldCallcallingSDKWrapperSetupCall() async throws {
+        _ = try await callingService.setupCall()
 
         XCTAssertTrue(callingSDKWrapper.setupCallWasCalled())
     }
 
-    func test_callingService_startCall_shouldCallcallingSDKWrapperStartCall() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
+    func test_callingService_startCall_shouldCallcallingSDKWrapperStartCall() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
 
         XCTAssertTrue(callingSDKWrapper.startCallWasCalled())
     }
 
-    func test_callingService_endCall_shouldCallCallingGatewayEndCall() {
-        _ = callingService.endCall()
+    func test_callingService_endCall_shouldCallCallingGatewayEndCall() async throws {
+        _ = try await callingService.endCall()
 
         XCTAssertTrue(callingSDKWrapper.endCallWasCalled())
     }
 
-    func test_callingService_mute_when_startCall_then_callWasMutedTrue() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
-        _ = callingService.muteLocalMic()
+    func test_callingService_mute_when_startCall_then_callWasMutedTrue() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
+        _ = try await callingService.muteLocalMic()
 
         XCTAssertTrue(callingSDKWrapper.muteWasCalled())
     }
 
-    func test_callingService_unmute_when_startCallAndMute_then_callWasUnmutedTrue() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
-        _ = callingService.muteLocalMic()
+    func test_callingService_unmute_when_startCallAndMute_then_callWasUnmutedTrue() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
+        _ = try await callingService.muteLocalMic()
 
         XCTAssertTrue(callingSDKWrapper.muteWasCalled())
 
-        _ = callingService.unmuteLocalMic()
+        _ = try await callingService.unmuteLocalMic()
 
         XCTAssertTrue(callingSDKWrapper.unmuteWasCalled())
     }
 
-    func test_callingService_startCall_when_joinCallCameraOptionOn_then_videoWasEnabled() {
-        _ = callingService.startCall(isCameraPreferred: true, isAudioPreferred: false)
+    func test_callingService_startCall_when_joinCallCameraOptionOn_then_videoWasEnabled() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: true, isAudioPreferred: false)
 
         XCTAssertTrue(callingSDKWrapper.videoEnabledWhenJoinCall())
     }
 
-    func test_callingService_startCall_when_joinCallCameraOptionOff_then_videoWasDisabled() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
+    func test_callingService_startCall_when_joinCallCameraOptionOff_then_videoWasDisabled() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
 
         XCTAssertFalse(callingSDKWrapper.videoEnabledWhenJoinCall())
     }
 
-    func test_callingService_startCall_when_joinCallMicOptionOn_then_callWasUnmuted() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: true)
+    func test_callingService_startCall_when_joinCallMicOptionOn_then_callWasUnmuted() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: true)
 
         XCTAssertFalse(callingSDKWrapper.mutedWhenJoinCall())
     }
 
-    func test_callingService_startCall_when_joinCallMicOptionOff_then_callWasMuted() {
-        _ = callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
+    func test_callingService_startCall_when_joinCallMicOptionOff_then_callWasMuted() async throws {
+        _ = try await callingService.startCall(isCameraPreferred: false, isAudioPreferred: false)
 
         XCTAssertTrue(callingSDKWrapper.mutedWhenJoinCall())
     }
 
-    func test_callingService_switchCamera_then_switchCameraWasCalled() {
-        _ = callingService.switchCamera()
+    func test_callingService_switchCamera_then_switchCameraWasCalled() async throws {
+        _ = try await callingService.switchCamera()
 
         XCTAssertTrue(callingSDKWrapper.switchCameraWasCalled())
     }
 
-    func test_callService_requestCameraPreviewOn_then_startPreviewVideoStreamCalled() {
-        _ = callingService.requestCameraPreviewOn()
+    func test_callService_requestCameraPreviewOn_then_startPreviewVideoStreamCalled() async throws {
+        _ = try await callingService.requestCameraPreviewOn()
         XCTAssertTrue(callingSDKWrapper.startPreviewVideoStreamCalled)
     }
 
-    func test_callingService_holdCall_then_holdCallWasCalled() {
-        _ = callingService.holdCall()
+    func test_callingService_holdCall_then_holdCallWasCalled() async throws {
+        _ = try await callingService.holdCall()
 
         XCTAssertTrue(callingSDKWrapper.holdCallCalled)
     }
 
-    func test_callingService_resumeCall_then_resumeCallWasCalled() {
-        _ = callingService.resumeCall()
+    func test_callingService_resumeCall_then_resumeCallWasCalled() async throws {
+        _ = try await callingService.resumeCall()
 
         XCTAssertTrue(callingSDKWrapper.resumeCallCalled)
     }
