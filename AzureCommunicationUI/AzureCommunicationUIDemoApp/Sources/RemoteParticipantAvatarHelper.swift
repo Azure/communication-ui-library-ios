3fbec89602029645a211792037e9b1c199d59528
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-08-29 15:53:18 -0700

    Remove unused code

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/RemoteParticipantAvatarHelper.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/RemoteParticipantAvatarHelper.swift
@@ -8,21 +8,6 @@ import AzureCommunicationCommon
 import AzureCommunicationUICalling
 
 struct RemoteParticipantAvatarHelper {
-//    private static func getRemoteParticipantId(_ identifier: CommunicationIdentifier) -> String? {
-//        switch identifier {
-//        case is CommunicationUserIdentifier:
-//            return (identifier as? CommunicationUserIdentifier)?.identifier
-//        case is UnknownIdentifier:
-//            return (identifier as? UnknownIdentifier)?.identifier
-//        case is PhoneNumberIdentifier:
-//            return (identifier as? PhoneNumberIdentifier)?.phoneNumber
-//        case is MicrosoftTeamsUserIdentifier:
-//            return (identifier as? MicrosoftTeamsUserIdentifier)?.userId
-//        default:
-//            return nil
-//        }
-//    }
-
     static func onRemoteParticipantJoined(to callComposite: CallComposite, identifiers: [CallIdentity]) {
         let avatars = ["cat", "fox", "koala", "monkey", "mouse", "octopus"]
         for identifier in identifiers {
