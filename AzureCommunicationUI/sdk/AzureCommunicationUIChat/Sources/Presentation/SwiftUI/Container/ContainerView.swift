7b14b5b3180f5add9a6213665f988656beea144f
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2022-09-02 14:06:52 -0700

    added skeleton for folder and files for chat and test

---
+++ AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/SwiftUI/Container/ContainerView.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+struct ContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
