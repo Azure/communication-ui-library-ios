15c4d24590966ba3bef6719bf1fbc019780ac184
Josh Lai <joshlai@microsoft.com>
2022-10-11 11:17:26 -0700

    [DiagnosticsManager] swift lint clean up

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Manager/DiagnosticsManager.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Manager/DiagnosticsManager.swift
@@ -1,6 +1,6 @@
 //
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//  Licensed under the MIT License.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import Combine
@@ -21,14 +21,14 @@ class DiagnosticsManager: DiagnosticsManagerProtocol {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] state in
                 self?.receive(state)
-        }.store(in: &cancellables)
+            }.store(in: &cancellables)
     }
-    
+
     private func receive(_ state: AppState) {
         // Receive the callid from callstate
         // Assign CallId to local property
     }
-    
+
     func getCallId() -> String {
         return ""
     }
