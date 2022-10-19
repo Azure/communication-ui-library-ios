a216cf477fb82e5835dafaa86d80141fda15d0ac
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-10-19 12:23:21 -0700

    Chat library as a swift package

---
+++ AzureCommunicationUI/AzureCommunicationUIChat/Sources/AzureCommunicationUIChat/Presentation/Manager/AppLifeCycleManager.swift
@@ -0,0 +1,45 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+@_spi(common) import AzureCommunicationUICommon
+import Combine
+import Foundation
+import UIKit
+
+protocol LifeCycleManagerProtocol {
+
+}
+
+class UIKitAppLifeCycleManager: LifeCycleManagerProtocol {
+
+    private let logger: Logger
+    private let store: Store<AppState, Action>
+
+    var cancellables = Set<AnyCancellable>()
+
+    init(store: Store<AppState, Action>,
+         logger: Logger) {
+        self.logger = logger
+        self.store = store
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(willDeactivate),
+                                               name: UIScene.willDeactivateNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didActivate),
+                                               name: UIScene.didActivateNotification,
+                                               object: nil)
+    }
+
+    @objc func willDeactivate(_ notification: Notification) {
+        logger.debug("Will Deactivate")
+        store.dispatch(action: .lifecycleAction(.backgroundEntered))
+    }
+
+    @objc func didActivate(_ notification: Notification) {
+        logger.debug("Did Activate")
+        store.dispatch(action: .lifecycleAction(.foregroundEntered))
+    }
+}
