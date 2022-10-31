b268218573b4dd8dca9756bfc39b72adde01d413
John <109105353+jpeng-ms@users.noreply.github.com>
2022-10-31 14:31:56 -0700

    Update ParticipantsReducer.swift

--- AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Redux/Reducer/ParticipantsReducer.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Redux/Reducer/ParticipantsReducer.swift
@@ -41,20 +41,6 @@ extension Reducer where State == ParticipantsState,
         case .participantsAction(.typingIndicatorReceived(let participant)):
             typingParticipants = typingParticipants.filter { $0.id != participant.id }
             typingParticipants.append(participant)
-            currentParticipants = [
-                "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-cf19-91b7-f0a7-923a0d00b6fd":
-                    ParticipantInfoModel(identifier: CommunicationUserIdentifier(
-                        "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-cf19-91b7-f0a7-923a0d00b6fd"),
-                                            displayName: "lucas"),
-                "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-cf19-7e00-f0a7-923a0d00b6fc":
-                    ParticipantInfoModel(identifier: CommunicationUserIdentifier(
-                        "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-cf19-7e00-f0a7-923a0d00b6fc"),
-                                            displayName: "johnny"),
-                "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-c645-8a65-5aad-923a0d009903":
-                    ParticipantInfoModel(identifier: CommunicationUserIdentifier(
-                        "8:acs:b6aada1f-0b1d-47ac-866f-91aae00a1d01_00000014-c645-8a65-5aad-923a0d009903"),
-                                        displayName: "John Smith")
-            ]
         case .participantsAction(.typingIndicatorExpired):
             typingParticipants = typingParticipants.filter(\.isTyping)
         case .repositoryAction(.chatMessageReceived(let message)):
