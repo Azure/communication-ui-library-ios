957fb4bbf0d0cbde504a3c22bec1dafff72934e6
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-11-01 11:22:43 -0700

    Tidy from PR review

--- AzureCommunicationUI/sdk/AzureCommunicationUICommon/Sources/AzureCommunicationUICommon/StringConstants.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICommon/Sources/AzureCommunicationUICommon/StringConstants.swift
@@ -6,9 +6,6 @@
 import Foundation
 
 enum StringConstants {
-    static let defaultEmptyName: String = "Unnamed participant"
-    static let localParticipantNamePostfix: String = "(me)"
-
     static let privacyPolicyLink: String = "https://privacy.microsoft.com/privacystatement#mainnoticetoendusersmodule"
     static let learnMoreLink: String = "https://support.microsoft.com/office/"
     + "record-a-meeting-in-teams-34dfbe7f-b07d-4a27-b4c6-de62f1348c24"
