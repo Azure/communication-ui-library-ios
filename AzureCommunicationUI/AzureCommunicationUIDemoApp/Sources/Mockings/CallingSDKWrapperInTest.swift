b12a6e2a3165d70a4826ef550bbd05e0756dd3cf
Kevin Guo <v-kevinguo@microsoft.com>
2022-11-15 14:31:06 -0800

    code clean up

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/Mockings/CallingSDKWrapperInTest.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/Mockings/CallingSDKWrapperInTest.swift
@@ -92,93 +92,34 @@ class CallingSDKWrapperInTest: NSObject, CallingSDKWrapperProtocol {
     public func getRemoteParticipant<ParticipantType, StreamType>(_ identifier: String)
     -> CompositeRemoteParticipant<ParticipantType, StreamType>? {
         return nil
-        // todo: will work on this in the next task
-//        guard let remote = findParticipant(identifier: identifier) else {
-//            return nil
-//        }
-//
-//        let remoteParticipant = AzureCommunicationCalling.RemoteParticipant
-//            .toCompositeRemoteParticipant(acsRemoteParticipant: remote)
-//        guard let castValue = remoteParticipant as? CompositeRemoteParticipant<ParticipantType, StreamType> else {
-//            return nil
-//        }
-//        return castValue
     }
 
     public func communicationIdForParticipant(identifier: String) -> CommunicationIdentifier? {
         return nil
-//        findParticipant(identifier: identifier)?.identifier
-    }
-
-    private func findParticipant(identifier: String) -> AzureCommunicationCalling.RemoteParticipant? {
-        return nil
-//        call?.remoteParticipants.first(where: { $0.identifier.stringValue == identifier })
     }
 
     public func getLocalVideoStream<LocalVideoStreamType>(_ identifier: String)
     -> CompositeLocalVideoStream<LocalVideoStreamType>? {
         return nil
-        // todo: will work on this in the next task
-//        guard getLocalVideoStreamIdentifier() == identifier else {
-//            return nil
-//        }
-//        guard let videoStream = localVideoStream,
-//              let castVideoStream = videoStream as? LocalVideoStreamType else {
-//            return nil
-//        }
-//        guard videoStream is LocalVideoStreamType else {
-//            return nil
-//        }
-//        return CompositeLocalVideoStream(
-//            mediaStreamType: videoStream.mediaStreamType.asCompositeMediaStreamType,
-//            wrappedObject: castVideoStream
-//        )
     }
 
     public func startCallLocalVideoStream() async throws -> String {
-        return ""
         // todo: will work on this in the next task
-//        let stream = await getValidLocalVideoStream()
-//        return try await startCallVideoStream(stream)
+        return ""
     }
 
     public func stopLocalVideoStream() async throws {
         // todo: will work on this in the next task
-//        guard let call = self.callMocking,
-//              let videoStream = self.localVideoStream else {
-//            logger.debug("Local video stopped successfully without call")
-//            return
-//        }
-//        do {
-//            try await call.stopVideo(stream: videoStream)
-//            logger.debug("Local video stopped successfully")
-//        } catch {
-//            logger.error( "Local video failed to stop. \(error)")
-//            throw error
-//        }
     }
 
     public func switchCamera() async throws -> CameraDevice {
         return .front
         // todo: will work on this in the next task
-//        guard let videoStream = localVideoStream else {
-//            let error = CallCompositeInternalError.cameraSwitchFailed
-//            logger.error("\(error)")
-//            throw error
-//        }
-//        let currentCamera = videoStream.source
-//        let flippedFacing: CameraFacing = currentCamera.cameraFacing == .front ? .back : .front
-//
-//        let deviceInfo = await getVideoDeviceInfo(flippedFacing)
-//        try await change(videoStream, source: deviceInfo)
-//        return flippedFacing.toCameraDevice()
     }
 
     public func startPreviewVideoStream() async throws -> String {
         return ""
         // todo: will work on this in the next task
-//        _ = await getValidLocalVideoStream()
-//        return getLocalVideoStreamIdentifier() ?? ""
     }
 
     public func muteLocalMic() async throws {
@@ -242,48 +183,6 @@ extension CallingSDKWrapperInTest {
         diagnostics.tags.append(contentsOf: appendingTag)
         clientOptionsMocking.diagnostics = diagnostics
         return CallClientMocking(options: clientOptionsMocking)
-    }
-
-    private func startCallVideoStream(
-        _ videoStream: AzureCommunicationCalling.LocalVideoStream
-    ) async throws -> String {
-        return ""
-
-        // todo: will work on this in the next task
-//        guard let call = self.callMocking else {
-//            let error = CallCompositeInternalError.cameraOnFailed
-//            self.logger.error( "Start call video stream failed")
-//            throw error
-//        }
-//        do {
-//            let localVideoStreamId = getLocalVideoStreamIdentifier() ?? ""
-//            try await call.startVideo(stream: videoStream)
-//            logger.debug("Local video started successfully")
-//            return localVideoStreamId
-//        } catch {
-//            logger.error( "Local video failed to start. \(error)")
-//            throw error
-//        }
-    }
-
-    private func change(_ videoStream: AzureCommunicationCalling.LocalVideoStream,
-                        source: VideoDeviceInfoMocking) async throws {
-        // todo: will work on this in the next task
-//        do {
-//            try await videoStream.switchSource(camera: source)
-//            logger.debug("Local video switched camera successfully")
-//        } catch {
-//            logger.error( "Local video failed to switch camera. \(error)")
-//            throw error
-//        }
-    }
-
-    private func getLocalVideoStreamIdentifier() -> String? {
-        // todo: will work on this in the next task
-        guard localVideoStream != nil else {
-            return nil
-        }
-        return "builtinCameraVideoStream"
     }
 }
 
