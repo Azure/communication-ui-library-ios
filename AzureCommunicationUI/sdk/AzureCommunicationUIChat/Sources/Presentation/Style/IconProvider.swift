3899fb858d85f1079ae4934be3dc7ffd68be6ab7
Patrick Latter <palatter@microsoft.com>
2022-11-17 15:54:04 -0800

    Consistent icon names

--- AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/Style/IconProvider.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/Style/IconProvider.swift
@@ -8,11 +8,11 @@ import FluentUI
 import SwiftUI
 
 enum CompositeIcon: String {
-    case leftArrow = "ic_ios_arrow_left_24"
+    case leftArrow = "ic_ios_arrow_left_24_filled"
     case downArrow = "ic_fluent_arrow_down_24_filled"
     case send = "ic_fluent_send_24_filled"
-    case systemJoin = "ic_fluent_person_add"
-    case systemLeave = "ic_fluent_person_remove"
+    case systemJoin = "ic_fluent_person_add_24_regular"
+    case systemLeave = "ic_fluent_person_remove_24_regular"
 }
 
 struct IconProvider {
