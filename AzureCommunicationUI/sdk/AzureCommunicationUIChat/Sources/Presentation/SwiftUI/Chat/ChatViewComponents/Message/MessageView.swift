4017fcc699e38028ef627267b98eac6780db02f8
Patrick Latter <73612854+palatter@users.noreply.github.com>
2022-12-21 15:42:13 -0800

    [Bugfix] Resolve issue with date header and text input box cursor padding (#548)

--- AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/SwiftUI/Chat/ChatViewComponents/Message/MessageView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/SwiftUI/Chat/ChatViewComponents/Message/MessageView.swift
@@ -6,18 +6,35 @@
 import SwiftUI
 
 struct MessageView: View {
+    private enum Constants {
+        static let horizontalPadding: CGFloat = 16
+        static let bottomPadding: CGFloat = 0
+        static let topPadding: CGFloat = 8
+        static let topConsecutivePadding: CGFloat = 4
+        static let remoteTrailingPadding: CGFloat = 60
+        static let messageWithSendStatusTrailingPadding: CGFloat = 1
+    }
+
     let messageModel: ChatMessageInfoModel
     let showDateHeader: Bool
     let isConsecutive: Bool
     let showUsername: Bool
     let showTime: Bool
+    let showMessageStatus: Bool
 
     // Inject localization with environment?
 
     var body: some View {
+        let edgeInsets = EdgeInsets(top: isConsecutive
+                                        ? Constants.topConsecutivePadding
+                                        : Constants.topPadding,
+                                    leading: Constants.horizontalPadding,
+                                    bottom: Constants.bottomPadding,
+                                    trailing: getMessageTrailingPadding(for: messageModel))
         VStack {
             dateHeader
             message
+            .padding(edgeInsets)
         }
     }
 
@@ -46,5 +63,17 @@ struct MessageView: View {
                 EmptyView()
             }
         }
+    }
+
+    private func getMessageTrailingPadding(for message: ChatMessageInfoModel) -> CGFloat {
+        if !message.isLocalUser {
+            return Constants.remoteTrailingPadding
+        }
+        if message.type == .text,
+           showMessageStatus,
+           message.sendStatus != nil {
+            return Constants.messageWithSendStatusTrailingPadding
+        }
+        return Constants.horizontalPadding
     }
 }
