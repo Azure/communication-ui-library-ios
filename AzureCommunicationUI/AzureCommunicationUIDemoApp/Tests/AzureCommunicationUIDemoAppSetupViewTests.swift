65b223acec398d751a6f38ca5c96fb558295c2e1
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-01-10 10:59:46 -0800

    Setup test update

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Tests/AzureCommunicationUIDemoAppSetupViewTests.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Tests/AzureCommunicationUIDemoAppSetupViewTests.swift
@@ -9,40 +9,25 @@ import XCTest
 class AzureCommunicationUIDemoAppSetupViewTests: XCUITestBase {
 
     // MARK: Setup view tests
-    func testCallCompositeAudioVideoSetup() {
+    func testCallCompositeSetupCallGroupCallSwiftUI() {
         tapInterfaceFor(.callSwiftUI)
-        startExperience(useCallingSDKMock: false)
+        if #unavailable(iOS 14.2) {
+            // there is an AppCenter issue for devices with iOS<14.2 when useCallingSDKMock = false
+            // however, the test can be run locally without any issues for all supported iOS versions
+            startExperience()
+        } else {
+            startExperience(useCallingSDKMock: false)
+        }
 
         wait(for: app.buttons[AccessibilityIdentifier.joinCallAccessibilityID.rawValue])
-
-        let videoButtonLabel = app.buttons[AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue].label
-        tapButton(accessibilityIdentifier: AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue)
 
         let audioButtonLabel = app.buttons[AccessibilityIdentifier.toggleMicAccessibilityID.rawValue].label
         tapButton(accessibilityIdentifier: AccessibilityIdentifier.toggleMicAccessibilityID.rawValue, shouldWait: true)
         check(predicate: NSPredicate(format: "label != %@", audioButtonLabel),
               for: app.buttons[AccessibilityIdentifier.toggleMicAccessibilityID.rawValue])
 
-        // check for camera button is moved to the end as
-        // video button invoke camera access alert on the first run and
-        // this alert isn't considered an interruption
-        // see Apple documentation for "Handling UI Interruptions" for more details
-        check(predicate: NSPredicate(format: "label != %@", videoButtonLabel),
-              for: app.buttons[AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue])
-
-        tapButton(accessibilityIdentifier: AccessibilityIdentifier.dismissButtonAccessibilityID.rawValue,
-                  shouldWait: true)
-        wait(for: app.buttons[AccessibilityId.startExperienceAccessibilityID.rawValue])
-    }
-
-    func testCallCompositeAudioDeviceSetup() {
-        tapInterfaceFor(.callSwiftUI)
-
-        // there is an AppCenter issue for devices with iOS<14.2 when useCallingSDKMock = false
-        // however, the test can be run locally without any issues for all supported iOS versions
-        startExperience(useCallingSDKMock: false)
-
-        wait(for: app.buttons[AccessibilityIdentifier.joinCallAccessibilityID.rawValue])
+        let videoButtonLabel = app.buttons[AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue].label
+        tapButton(accessibilityIdentifier: AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue)
 
         let audioDeviceButtonValue = app.buttons[AccessibilityIdentifier.toggleAudioDeviceAccessibilityID.rawValue].value as? String
         tapButton(accessibilityIdentifier: AccessibilityIdentifier.toggleAudioDeviceAccessibilityID.rawValue,
@@ -56,6 +41,13 @@ class AzureCommunicationUIDemoAppSetupViewTests: XCUITestBase {
         }
         check(predicate: NSPredicate(format: "value != %@", audioDeviceButtonValue ?? ""),
               for: app.buttons[AccessibilityIdentifier.toggleAudioDeviceAccessibilityID.rawValue])
+
+        // check for camera button is moved to the end as
+        // video button invoke camera access alert on the first run and
+        // this alert isn't considered an interruption
+        // see Apple documentation for "Handling UI Interruptions" for more details
+        check(predicate: NSPredicate(format: "label != %@", videoButtonLabel),
+              for: app.buttons[AccessibilityIdentifier.toggleVideoAccessibilityID.rawValue])
 
         tapButton(accessibilityIdentifier: AccessibilityIdentifier.dismissButtonAccessibilityID.rawValue,
                   shouldWait: true)
