9373a12845cfdff9231a9648557d02368ceb664e
John <109105353+jpeng-ms@users.noreply.github.com>
2023-01-16 11:49:36 -0800

    update arguments

--- scripts/podspec-updater.py
+++ scripts/podspec-updater.py
@@ -13,6 +13,8 @@ class Composite(Enum):
 				return member
 
 new_version = ''
+swift_version = ''
+platform_version = ''
 selectedComposite = Composite('')
 
 # constants
@@ -22,11 +24,9 @@ short_version_pattern = re.compile(r"[0-9]+\.[0-9]+", re.IGNORECASE)
 
 # calling
 calling_podspec = 'AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICalling.podspec'
-calling_pbx_path = 'AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUICalling.xcodeproj/project.pbxproj'
 
 # chat
 chat_podspec = 'AzureCommunicationUI/sdk/AzureCommunicationUICalling/AzureCommunicationUIChat.podspec'
-chat_pbx_path = 'AzureCommunicationUI/sdk/AzureCommunicationUIChat/AzureCommunicationUIChat.xcodeproj/project.pbxproj'
 
 
 def update(path, replaceFrom, replaceTo):
@@ -50,50 +50,63 @@ def update_version():
 	print('Step 1 of 3, spec.version updated to ' + new_version + ' from ' + current_version)
 
 def update_swift():
-	local_path_pbx = calling_pbx_path if selectedComposite == Composite.CALLING else chat_pbx_path
+	if swift_version == '':
+		print('Step 2 of 3. Skipped')
+		return
 	local_path_pod = calling_podspec if selectedComposite == Composite.CALLING else chat_podspec
 	with open(acs_UI_library_Path + local_path_pod, 'r') as fi_pod:
 		s_pod = fi_pod.read()
 	segment = extract_string(s_pod, 'spec.swift_version', '\n')
 	current_version = short_version_pattern.search(segment).group()
-	with open(acs_UI_library_Path + local_path_pbx, 'r') as fi_pbx:
-		s_pbx = fi_pbx.read()
-	segment = extract_string(s_pbx, 'SWIFT_VERSION', '\n')
-	new_version = short_version_pattern.findall(segment)[0]
+	new_version = swift_version
 	update(acs_UI_library_Path + local_path_pod, current_version, new_version)
 	print('Step 2 of 3, spec.swift_version updated to ' + new_version + ' from ' + current_version)
 
 def update_platform():
-	local_path_pbx = calling_pbx_path if selectedComposite == Composite.CALLING else chat_pbx_path
+	if platform_version == '':
+		print('Step 3 of 3. Skipped')
+		return
 	local_path_pod = calling_podspec if selectedComposite == Composite.CALLING else chat_podspec
 	with open(acs_UI_library_Path + local_path_pod, 'r') as fi_pod:
 		s_pod = fi_pod.read()
 	segment = extract_string(s_pod, 'spec.platform', '\n')
 	current_version = short_version_pattern.search(segment).group()
-	with open(acs_UI_library_Path + local_path_pbx, 'r') as fi_pbx:
-		s1 = fi_pbx.read()
-	segment = extract_string(s1, 'IPHONEOS_DEPLOYMENT_TARGET', '\n')
-	new_version = short_version_pattern.findall(segment)[0]
+	new_version = platform_version
 	update(acs_UI_library_Path + local_path_pod, current_version, new_version)
 	print('Step 3 of 3, spec.platform updated to ' + new_version + ' from ' + current_version)
 	
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv, "hv:c:", ["help", "version=", "composite="])
+		opts, args = getopt.getopt(argv, "hv:c:s:p:", ["help", "version=", "composite=", "swift=", "platform="])
 	except getopt.GetoptError:
 		sys.exit('Inputs are missing ' +
-			'Usage: main-repo-updater.py -c COMPOSITE -v NEW_VERSION, where COMPOSITE should be either calling or chat.')
+			'Usage: main-repo-updater.py -c COMPOSITE -v NEW_VERSION -s SWIFT_VERSION -p PLATFORM_VERSION, '+ 
+			'where COMPOSITE should be either calling or chat. And -s and -p are optional.')
 	for opt, arg in opts:
 		if opt == '-h':
-			sys.exit('Expected parameters: 1. -c or --composite of options of CALLING or CHAT 2. -v or --version')
-		elif opt in ('-v', '--version'):
+			sys.exit('Expected parameters: ' + 
+				'1. -c or --composite of options of \'calling\' or \'chat\' ' + 
+				'2. -v or --version ' + 
+				'3. (optional) -s or --swift ' + 
+				'4. (optional) -p or --platform ')
+		elif opt in ('-v', '--version '):
 			global new_version 
 			new_version = arg
 		elif opt in ('-c', '--composite'):
 			global selectedComposite 
 			selectedComposite = Composite(arg)
+		elif opt in ('-s', '--swift'):
+			global swift_version 
+			swift_version = arg
+		elif opt in ('-p', '--platform'):
+			global platform_version 
+			platform_version = arg
 	if selectedComposite == Composite.UNKNOWN:
-		sys.exit('Composite is Unknown. Supported Composites are \'calling\' and \'chat\'.')
+		sys.exit('Error: Composite is Unknown. Supported Composites are \'calling\' and \'chat\'.')
+	if swift_version == '':
+		print('Warning: No swift version given. Swift version update would be skipped.')
+	if platform_version == '':
+		print("Warning: No minimum platform version given. Platform update would be skipped. ")
 	update_version()
 	update_swift()
 	update_platform()
