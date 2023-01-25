ca31925db31e4527f97ed938830e267692f2b094
Patrick Latter <palatter@microsoft.com>
2023-01-25 15:57:42 -0800

    Fix padding on typing participants

--- AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/SwiftUI/Chat/ChatView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUIChat/Sources/Presentation/SwiftUI/Chat/ChatView.swift
@@ -6,12 +6,19 @@
 import SwiftUI
 
 struct ChatView: View {
+    private enum Constants {
+        static let messageListBottomPadding: CGFloat = 12
+        static let typingParticipantsBottomPadding: CGFloat = 12
+    }
+
     @StateObject var viewModel: ChatViewModel
 
     var body: some View {
         VStack(spacing: 0) {
             MessageListView(viewModel: viewModel.messageListViewModel)
+                .padding(.bottom, Constants.messageListBottomPadding )
             TypingParticipantsView(viewModel: viewModel.typingParticipantsViewModel)
+                .padding(.bottom, Constants.typingParticipantsBottomPadding)
             BottomBarView(viewModel: viewModel.bottomBarViewModel)
         }
         .onAppear {
