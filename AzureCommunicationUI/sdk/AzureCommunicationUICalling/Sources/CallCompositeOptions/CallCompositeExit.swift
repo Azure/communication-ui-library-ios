3334e448d4621066f65548e4d80898a2aedffbad
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2023-04-30 11:14:10 -0700

    [Calling][Feature] api for exit composite and call state change

---
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallCompositeExit.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+
+/// The error thrown after Call Composite launching.
+public struct CallCompositeExit {
+    /// The string representing the CallCompositeErrorCode.
+    public let code: String
+
+    /// The NSError returned from Azure Communication SDK.
+    public let error: Error?
+}
+
+extension CallCompositeExit: Equatable {
+    public static func == (lhs: CallCompositeExit, rhs: CallCompositeExit) -> Bool {
+        if let error1 = lhs.error as NSError?,
+           let error2 = rhs.error as NSError? {
+            return error1.domain == error2.domain
+                && error1.code == error2.code
+                && "\(error1.description)" == "\(error2.description)"
+                && lhs.code == rhs.code
+        }
+
+        return false
+    }
+}
