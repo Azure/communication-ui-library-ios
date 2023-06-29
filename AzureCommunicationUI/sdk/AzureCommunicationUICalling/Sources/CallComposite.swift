72c1c99dc8abd348ffe0679fc4bd226bad455e65
Pavel Prystinka <pprystinka@microsoft.com>
2023-06-28 18:40:30 -0700

    rename show method to displayCallCompositeIfWasHidden

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallComposite.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallComposite.swift
@@ -128,9 +128,10 @@ public class CallComposite {
                           completionHandler: completionHandler)
     }
 
-    public func show() {
+    /// Display Call Composite if it was hidden by user going Back in navigation while on the call.
+    public func displayCallCompositeIfWasHidden() {
         guard let store = self.store, let viewFactory = self.viewFactory else {
-            logger.error("CallComposite was not launched yet. launch method has to be called first")
+            logger.error("CallComposite was not launched yet. launch() method has to be called first.")
             return
         }
         self.pipManager?.stopPictureInPicture()
@@ -315,7 +316,7 @@ extension CallComposite {
                                      onPipStoped: {
 //            self.logger.debug("onPipStoped")
             self.pipViewController?.dismissSelf()
-            self.show()
+            self.displayCallCompositeIfWasHidden()
         },
                                      onPipStartFailed: {
 //            self.logger.debug("onPipStartFailed")
