01b13bdf95c59f80e4792d8c9af3c1e016b78376
adamhammer <adamhammer@microsoft.com>
2023-07-07 16:38:16 -0700

    added version to the 3 dot

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/ViewComponents/Drawer/MoreCallOptionsListViewModel.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/ViewComponents/Drawer/MoreCallOptionsListViewModel.swift
@@ -17,12 +17,24 @@ class MoreCallOptionsListViewModel: ObservableObject {
          showSharingViewAction: @escaping () -> Void) {
         self.compositeViewModelFactory = compositeViewModelFactory
         self.localizationProvider = localizationProvider
-
         let shareDebugInfoModel = compositeViewModelFactory.makeDrawerListItemViewModel(
             icon: .share,
             title: localizationProvider.getLocalizedString(.shareDiagnosticsInfo),
             accessibilityIdentifier: AccessibilityIdentifier.shareDiagnosticsAccessibilityID.rawValue,
             action: showSharingViewAction)
-        items = [shareDebugInfoModel]
+
+        // Obtain version string from the library's bundle
+        let version = Bundle(for: Self.self).infoDictionary?["CFBundleShortVersionString"] as? String
+            ?? "Version not found"
+
+        // Create a new DrawerListItemViewModel for the version
+        let versionInfoModel = compositeViewModelFactory.makeDrawerListItemViewModel(
+            icon: .checkmark,  // Use an appropriate icon for version information
+            title: "Library version: \(version)",
+            accessibilityIdentifier: AccessibilityIdentifier.shareDiagnosticsAccessibilityID.rawValue,
+            action: {})  // You may not need an action for this, but if you do, provide one
+
+        // Add both models to the items array
+        items = [versionInfoModel, shareDebugInfoModel]
     }
 }
