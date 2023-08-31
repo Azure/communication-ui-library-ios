0e2f07aacb8e61bd50f5cc4cbfbdfc74e957345c
Luciano Almeida <lucianopa@microsoft.com>
2023-08-31 13:09:39 -0300

    [UFD] Fixing fowarding subjects to diagnostic events

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Model/UserFacingDiagnosticModel.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Model/UserFacingDiagnosticModel.swift
@@ -5,29 +5,29 @@
 
 import AzureCommunicationCalling
 
-enum NetworkDiagnostic: String {
-    case networkReconnectionQuality = "network_reconnection_quality"
-    case networkReceiveQuality = "network_receive_quality"
-    case networkSendQuality = "network_send_quality"
-    case networkUnavailable = "network_unavailable"
-    case networkRelaysUnreachable = "network_relays_unreachable"
+enum NetworkDiagnostic {
+    case networkReconnectionQuality
+    case networkReceiveQuality
+    case networkSendQuality
+    case networkUnavailable
+    case networkRelaysUnreachable
 }
 
-enum MediaDiagnostic: String {
-    case speakerNotFunctioning = "speaker_not_functioning"
-    case speakerBusy = "speaker_busy"
-    case speakerMuted = "speaker_muted"
-    case speakerVolumeZero = "speaker_volume_zero"
-    case noSpeakerDevicesAvailable = "no_speaker_devices_available"
-    case speakingWhileMicrophoneIsMuted = "speaking_while_microphone_is_muted"
-    case noMicrophoneDevicesAvaliable = "no_microphone_devices_available"
-    case microphoneBusy = "microphone_busy"
-    case cameraFrozen = "camera_frozen"
-    case cameraStartFailed = "camera_start_failed"
-    case cameraStartTimedOut = "camera_start_timed_out"
-    case microphoneNotFunctioning = "microphone_not_functioning"
-    case microphoneMutedUnexpectedly = "microphone_muted_unexpectedly"
-    case cameraPermissionDenied = "camera_permission_denied"
+enum MediaDiagnostic {
+    case speakerNotFunctioning
+    case speakerBusy
+    case speakerMuted
+    case speakerVolumeZero
+    case noSpeakerDevicesAvailable
+    case speakingWhileMicrophoneIsMuted
+    case noMicrophoneDevicesAvailable
+    case microphoneBusy
+    case cameraFrozen
+    case cameraStartFailed
+    case cameraStartTimedOut
+    case microphoneNotFunctioning
+    case microphoneMutedUnexpectedly
+    case cameraPermissionDenied
 }
 
 struct NetworkQualityDiagnosticModel {
