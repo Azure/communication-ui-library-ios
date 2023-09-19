03258e7bf66f757e88d423694b431c471847f9e6
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2023-09-19 15:24:56 -0700

    [Calling] Filter our InLobby and Disconnected users from UI (#736)

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Builder/ParticipantInfoModelBuilder.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Builder/ParticipantInfoModelBuilder.swift
@@ -12,7 +12,8 @@ struct ParticipantInfoModelBuilder {
                     screenShareStreamId: String? = nil,
                     displayName: String = "displayName",
                     isSpeaking: Bool = false,
-                    isMuted: Bool = true) -> ParticipantInfoModel {
+                    isMuted: Bool = true,
+                    status: ParticipantStatus = .idle) -> ParticipantInfoModel {
         var videoStreamInfoModel: VideoStreamInfoModel?
         var screenShareIdInfoModel: VideoStreamInfoModel?
         if let screenShareId = screenShareStreamId {
@@ -30,7 +31,7 @@ struct ParticipantInfoModelBuilder {
                                     isMuted: isMuted,
                                     isRemoteUser: true,
                                     userIdentifier: participantIdentifier,
-                                    status: .idle,
+                                    status: status,
                                     screenShareVideoStreamModel: screenShareIdInfoModel,
                                     cameraVideoStreamModel: videoStreamInfoModel)
     }
