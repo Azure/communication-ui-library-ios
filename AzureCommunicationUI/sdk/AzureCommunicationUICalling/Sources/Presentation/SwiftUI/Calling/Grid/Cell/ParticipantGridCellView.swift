92d7a6dfc12a28d5c46f62dc5d581327f5220c43
Pavel Prystinka <pprystinka@microsoft.com>
2023-09-20 12:12:53 -0700

    Do not turn incoming video off when in PiP and app is in background

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/Grid/Cell/ParticipantGridCellView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/Grid/Cell/ParticipantGridCellView.swift
@@ -19,8 +19,7 @@ struct ParticipantGridCellView: View {
     var body: some View {
         Group {
             GeometryReader { geometry in
-                if !viewModel.isInBackground,
-                   let videoStreamId = displayedVideoStreamId,
+                if let videoStreamId = displayedVideoStreamId,
                    let rendererViewInfo = getRendererViewInfo(for: videoStreamId) {
                     let zoomable = viewModel.videoViewModel?.videoStreamType == .screenSharing
                     ParticipantGridCellVideoView(videoRendererViewInfo: rendererViewInfo,
