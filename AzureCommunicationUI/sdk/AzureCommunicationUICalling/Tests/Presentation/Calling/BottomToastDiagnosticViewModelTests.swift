4b7eec852d3a3987d86127c9c818bb030eea4205
Luciano Almeida <lucianopa@microsoft.com>
2023-09-21 16:32:55 -0300

    [UFD] Handle all bottom toast diagnostics and add proper tests

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Presentation/Calling/BottomToastDiagnosticViewModelTests.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Presentation/Calling/BottomToastDiagnosticViewModelTests.swift
@@ -26,32 +26,52 @@ class BottomToastDiagnosticViewModelTests: XCTestCase {
         XCTAssertEqual(sut.icon, CompositeIcon.micOff)
     }
 
+    func test_that_presenting_camera_start_failed_diagnostic_bottom_toast_shows_title_and_icon() {
+        let sut = makeSUT(mediaDiagnostic: .cameraStartFailed, localizationProvider: localizationProvider)
+        XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.CameraNotWorking")
+        XCTAssertEqual(sut.icon, CompositeIcon.videoOff)
+    }
+
+    func test_that_presenting_camera_start_timed_out_diagnostic_bottom_toast_shows_title_and_icon() {
+        let sut = makeSUT(mediaDiagnostic: .cameraStartTimedOut, localizationProvider: localizationProvider)
+        XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.CameraNotWorking")
+        XCTAssertEqual(sut.icon, CompositeIcon.videoOff)
+    }
+
     func test_that_presenting_network_receive_quality_bottom_toast_shows_title_and_icon() {
         let sut = makeSUT(networkDiagnostic: .networkReceiveQuality, localizationProvider: localizationProvider)
         XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.NetworkQualityLow")
-        XCTAssertEqual(sut.icon, nil)
+        XCTAssertEqual(sut.icon, CompositeIcon.wifiWarning)
     }
 
     func test_that_presenting_network_send_quality_bottom_toast_shows_title_and_icon() {
         let sut = makeSUT(networkDiagnostic: .networkSendQuality, localizationProvider: localizationProvider)
         XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.NetworkQualityLow")
-        XCTAssertEqual(sut.icon, nil)
+        XCTAssertEqual(sut.icon, CompositeIcon.wifiWarning)
+    }
+
+    func test_that_presenting_network_unavailable_bottom_toast_shows_title_and_icon() {
+        let sut = makeSUT(networkDiagnostic: .networkUnavailable, localizationProvider: localizationProvider)
+        XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.NetworkLost")
+        XCTAssertEqual(sut.icon, CompositeIcon.wifiWarning)
+    }
+
+    func test_that_presenting_network_relays_unrachable_bottom_toast_shows_title_and_icon() {
+        let sut = makeSUT(networkDiagnostic: .networkRelaysUnreachable, localizationProvider: localizationProvider)
+        XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.NetworkLost")
+        XCTAssertEqual(sut.icon, CompositeIcon.wifiWarning)
+    }
+
+    func test_that_presenting_network_reconnection_quality_bottom_toast_shows_title_and_icon() {
+        let sut = makeSUT(networkDiagnostic: .networkReconnectionQuality, localizationProvider: localizationProvider)
+        XCTAssertEqual(sut.text, "AzureCommunicationUICalling.Diagnostics.Text.NetworkReconnect")
+        XCTAssertEqual(sut.icon, CompositeIcon.wifiWarning)
     }
 
     func test_that_unhadled_media_diagnostics_bottom_toast_dont_present_any_text() {
-        let otherMediaDiagnostics = MediaCallDiagnostic.allCases.filter({ $0 != .speakingWhileMicrophoneIsMuted })
+        let otherMediaDiagnostics = MediaCallDiagnostic.allCases.filter({ !BottomToastDiagnosticViewModel.handledMediaDiagnostics.contains($0) })
         for diagnostic in otherMediaDiagnostics {
             let sut = makeSUT(mediaDiagnostic: diagnostic, localizationProvider: localizationProvider)
-            XCTAssertEqual(sut.text, "")
-            XCTAssertEqual(sut.icon, nil)
-        }
-    }
-
-    func test_that_unhadled_network_diagnostics_bottom_toast_dont_present_any_text() {
-        let otherNetworkDiagnostics = NetworkCallDiagnostic.allCases
-            .filter({ $0 != .networkSendQuality &&  $0 != .networkReceiveQuality})
-        for diagnostic in otherNetworkDiagnostics {
-            let sut = makeSUT(networkDiagnostic: diagnostic, localizationProvider: localizationProvider)
             XCTAssertEqual(sut.text, "")
             XCTAssertEqual(sut.icon, nil)
         }
