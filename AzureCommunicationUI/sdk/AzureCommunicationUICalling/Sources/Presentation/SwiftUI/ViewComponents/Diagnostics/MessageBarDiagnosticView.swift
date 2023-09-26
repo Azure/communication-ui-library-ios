e008ee30e5ece8cb3111d818e2189fa5dd571b58
Luciano Almeida <lucianopa@microsoft.com>
2023-09-26 14:50:09 -0300

    [UFD] Update colors for call diagnostics

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/ViewComponents/Diagnostics/MessageBarDiagnosticView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/ViewComponents/Diagnostics/MessageBarDiagnosticView.swift
@@ -10,6 +10,7 @@ struct MessageBarDiagnosticView: View {
     @ObservedObject var viewModel: MessageBarDiagnosticViewModel
 
     private let cornerRadius: CGFloat = 6
+    private let foregroundColor: Color = .white
 
     var body: some View {
         if viewModel.isDisplayed {
@@ -17,14 +18,14 @@ struct MessageBarDiagnosticView: View {
                 if let icon = viewModel.icon {
                     IconProvider().getImage(for: icon)
                         .frame(width: 16, height: 16)
-                        .foregroundColor(Color(StyleProvider.color.onSurface))
+                        .foregroundColor(foregroundColor)
                         .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
                 }
                 Text(viewModel.text)
                     .font(Fonts.subhead.font)
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
                     .multilineTextAlignment(.center)
-                    .foregroundColor(Color(StyleProvider.color.onSurface))
+                    .foregroundColor(foregroundColor)
                     .accessibilitySortPriority(2)
 
                 Spacer()
@@ -33,7 +34,7 @@ struct MessageBarDiagnosticView: View {
                 }, label: {
                     IconProvider().getImage(for: .dismiss)
                         .frame(width: 16, height: 16)
-                        .foregroundColor(Color(StyleProvider.color.onSurface))
+                        .foregroundColor(foregroundColor)
                 })
                 .padding([.top, .bottom, .trailing])
                 .accessibilityLabel(Text(viewModel.dismissAccessibilitylabel))
@@ -41,7 +42,7 @@ struct MessageBarDiagnosticView: View {
                 .accessibilitySortPriority(0)
             }
             .frame(height: 48)
-            .background(Color(StyleProvider.color.backgroundColor))
+            .background(Color(StyleProvider.color.surfaceDarkColor))
             .cornerRadius(cornerRadius)
         }
     }
