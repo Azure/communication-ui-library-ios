0db77ba0f2f197dd47e00ced4016b141dd5ef652
Pavel Prystinka <pprystinka@microsoft.com>
2024-01-24 11:27:41 -0800

    Hide selfie avatar and camera when in PiP

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingView.swift
@@ -72,7 +72,7 @@ struct CallingView: View {
                 ZStack(alignment: .bottomTrailing) {
                     videoGridView
                         .accessibilityHidden(!viewModel.isVideoGridViewAccessibilityAvailable)
-                    if viewModel.isParticipantGridDisplayed {
+                    if viewModel.isParticipantGridDisplayed && !viewModel.isInPip {
                         Group {
                             DraggableLocalVideoView(containerBounds:
                                                         geometry.frame(in: .local),
