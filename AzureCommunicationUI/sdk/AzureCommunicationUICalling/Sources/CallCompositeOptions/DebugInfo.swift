3c3e3c8006bbc965305b471b018f08809136287e
adamhammer <adamhammer@microsoft.com>
2024-02-09 17:36:31 -0800

    improved swiftdoc

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/DebugInfo.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/DebugInfo.swift
@@ -5,22 +5,37 @@
 
 import Foundation
 
-/// A Call Composite debug information.
+/// Encapsulates debug information for an ACS Call Composite, including call history, SDK version, and log files.
 public struct DebugInfo {
-    /// The history of calls up to 30 days. Ordered ascending by call started date.
+    /// The history of calls, stored as an array of `CallHistoryRecord` objects, covering up to the last 30 days.
+    /// These records are sorted in ascending order based on the date each call was started.
     public let callHistoryRecords: [CallHistoryRecord]
+
+    /// A string representing the version of the ACS Calling UI SDK currently in use.
     public let callingUIVersion: String
+
+    /// An array of `URL` objects, each pointing to a current log file of the ACS Calling Composite.
+    /// These files are intended for use in debugging and providing support.
     public let logFiles: [URL]
-    // Take a screenshot
+
+    /// Attempts to capture a screenshot of the current call composite interface.
+    ///
+    /// - Returns: A `URL` pointing to the saved screenshot image in the temporary/cache directory,
+    ///            or `nil` if it was not possible to take a screenshot.
     func takeScreenshot() -> URL? {
-        guard let screenshotImage = captureScreenshot()
-        else { return nil }
+        guard let screenshotImage = captureScreenshot() else {
+            return nil
+        }
         return saveScreenshot(screenshotImage)
     }
-    /// Call history.
-    init(callHistoryRecords: [CallHistoryRecord],
-         callingUIVersion: String,
-         logFiles: [URL]) {
+
+    /// Initializes a new `DebugInfo` with the specified call history records, calling UI SDK version, and log files.
+    ///
+    /// - Parameters:
+    ///   - callHistoryRecords: An array of `CallHistoryRecord` objects representing the call history.
+    ///   - callingUIVersion: The version of the ACS Calling UI SDK.
+    ///   - logFiles: An array of `URL` objects pointing to log files.
+    init(callHistoryRecords: [CallHistoryRecord], callingUIVersion: String, logFiles: [URL]) {
         self.callHistoryRecords = callHistoryRecords
         self.callingUIVersion = callingUIVersion
         self.logFiles = logFiles
