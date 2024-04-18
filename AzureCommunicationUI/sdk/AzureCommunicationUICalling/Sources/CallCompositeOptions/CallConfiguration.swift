b2ad2d9fb9ba49073072f7633b7399134e081978
adamhammer <adamhammer@microsoft.com>
2024-04-18 15:44:31 -0700

    more disabling

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallConfiguration.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallConfiguration.swift
@@ -13,32 +13,37 @@ struct CallConfiguration {
     let credential: CommunicationTokenCredential
     let displayName: String?
     let diagnosticConfig: DiagnosticConfig
+    /* <ROOMS_SUPPORT:7>
     let roomId: String?
     let roomRoleHint: ParticipantRole?
-
+    </ROOMS_SUPPORT> */
     init(locator: JoinLocator,
          credential: CommunicationTokenCredential,
-         displayName: String?,
-         roleHint: ParticipantRole?) {
+         displayName: String? /* <ROOMS_SUPPORT> ,
+         roleHint: ParticipantRole? </ROOMS_SUPPORT> */) {
         switch locator {
         case let .groupCall(groupId: groupId):
             self.groupId = groupId
             self.meetingLink = nil
+            /* <ROOMS_SUPPORT>
             self.roomId = nil
             self.roomRoleHint = nil
+            </ROOMS_SUPPORT> */
             self.compositeCallType = .groupCall
         case let .teamsMeeting(teamsLink: meetingLink):
             self.groupId = nil
             self.meetingLink = meetingLink
+            self.compositeCallType = .teamsMeeting
+        /* <ROOMS_SUPPORT>
             self.roomId = nil
             self.roomRoleHint = nil
-            self.compositeCallType = .teamsMeeting
         case let .roomCall(roomId: roomId):
             self.roomId = roomId
             self.roomRoleHint = roleHint
             self.groupId = nil
             self.meetingLink = nil
             self.compositeCallType = .roomsCall
+        </ROOMS_SUPPORT> */
         }
         self.credential = credential
         self.displayName = displayName
@@ -49,5 +54,5 @@ struct CallConfiguration {
 enum CompositeCallType {
     case groupCall
     case teamsMeeting
-    case roomsCall
+    /* <ROOMS_SUPPORT:3> case roomsCall </ROOMS_SUPPORT:1> */
 }
--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/LocalOptions.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/LocalOptions.swift
@@ -14,11 +14,14 @@ public struct LocalOptions {
     /// Configuration for the call setup screen, including titles and subtitles.
     let setupScreenViewData: SetupScreenViewData?
 
+    /* <ROOMS_SUPPORT>
     /// Hint the role of the user when the role is not available before a Rooms call is started.
     /// This value should be obtained using the Rooms API. This role will determine permissions in the
     /// Setup screen of the CallComposite.
     /// The true role of the user will be synced with ACS services when a Rooms call starts.
     let roleHint: ParticipantRole?
+    </ROOMS_SUPPORT> */
+
     ///  The CameraOn is used when we skip the setup screen
 
     /// Determines if the microphone is enabled upon joining the call, bypassing the setup screen.
@@ -50,15 +53,15 @@ public struct LocalOptions {
                 cameraOn: Bool? = false,
                 microphoneOn: Bool? = false,
                 skipSetupScreen: Bool? = false,
-                audioVideoMode: CallCompositeAudioVideoMode = .audioAndVideo,
-                roleHint: ParticipantRole? = nil) {
+                audioVideoMode: CallCompositeAudioVideoMode = .audioAndVideo /* <ROOMS_SUPPORT> ,
+                roleHint: ParticipantRole? = nil </ROOMS_SUPPORT> */) {
         self.participantViewData = participantViewData
         self.setupScreenViewData = setupScreenViewData
         self.cameraOnInternal = cameraOn
         self.microphoneOn = microphoneOn
         self.skipSetupScreen = skipSetupScreen
         self.audioVideoMode = audioVideoMode
-        self.roleHint = roleHint
+        /* <ROOMS_SUPPORT> self.roleHint = roleHint </ROOMS_SUPPORT> */
     }
 
     /// Determines the actual state of the camera
