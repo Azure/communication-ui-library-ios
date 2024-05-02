05d1d41dab3e98866178a8d37c5ed7439c5a66e5
iaulakh <iaulakh@microsoft.com>
2024-05-01 19:35:34 -0700

    Fix public API

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/LeaveCallConfirmationMode.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/LeaveCallConfirmationMode.swift
@@ -4,23 +4,54 @@
 //
 
 import Foundation
+import AzureCore
 
 /// Enum defining options for leaving a call confirmation.
-public enum LeaveCallConfirmationMode {
-    /// Enables the leave call confirmation.
-    case alwaysEnabled
-    /// Disables the leave call confirmation.
-    case alwaysDisabled
+public struct LeaveCallConfirmationMode: Equatable, RequestStringConvertible {
+    internal enum  LeaveCallConfirmationModeKV {
+        case alwaysEnabled
+        case alwaysDisabled
+        case unknown(String)
 
-    /// Provides a `String` representation of the mode.
-    /// - For `.always_enabled`, returns "always_enabled".
-    /// - For `.always_disabled`, returns "always_disabled".
-    var rawValue: String {
-        switch self {
-        case .alwaysEnabled:
-            return "always_enabled"
-        case .alwaysDisabled:
-            return "always_disabled"
+        var rawValue: String {
+            switch self {
+            case .alwaysEnabled:
+                return "always_enabled"
+            case .alwaysDisabled:
+                return "always_disabled"
+            case .unknown(let value):
+                return value
+            }
+        }
+        init(rawValue: String) {
+            switch rawValue.lowercased() {
+            case "always_enabled":
+                self = .alwaysEnabled
+            case "always_disabled":
+                self = .alwaysDisabled
+            default:
+                self = .unknown(rawValue.lowercased())
+            }
         }
     }
+
+    private let value: LeaveCallConfirmationModeKV
+
+    public var requestString: String {
+        return value.rawValue
+    }
+
+    private init(rawValue: String) {
+        self.value = LeaveCallConfirmationModeKV(rawValue: rawValue)
+    }
+
+    public static func == (lhs: LeaveCallConfirmationMode, rhs: LeaveCallConfirmationMode) -> Bool {
+        return lhs.requestString == rhs.requestString
+    }
+
+    /// Enables the leave call confirmation.
+    public static let alwaysEnabled: LeaveCallConfirmationMode = .init(rawValue: "always_enabled")
+
+    /// Disables the leave call confirmation.
+    public static let alwaysDisabled: LeaveCallConfirmationMode = .init(rawValue: "always_disabled")
 }
