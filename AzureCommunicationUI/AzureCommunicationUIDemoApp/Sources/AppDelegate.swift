9c6fe5fe6f2d4edd564a4fbd2e2c5f0f37292fdb
Adam Hammer <adamhammer2@gmail.com>
2024-05-06 13:38:57 -0700

    Update AppDelegate.swift

--- AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/AppDelegate.swift
+++ AzureCommunicationUI/AzureCommunicationUIDemoApp/Sources/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch. (HELLO WORLD)
 
         AppCenter.start(withAppSecret: envConfigSubject.appCenterSecret, services: [Crashes.self])
 
