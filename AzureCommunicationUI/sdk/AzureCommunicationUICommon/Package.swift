709c0cc8853365e6a6aadda5d737a95d8e6229cb
adamhammer <adamhammer@microsoft.com>
2024-05-08 15:48:27 -0700

    added swiftlint ignore for common package.swift

--- AzureCommunicationUI/sdk/AzureCommunicationUICommon/Package.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICommon/Package.swift
@@ -1,8 +1,3 @@
-//
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//  Licensed under the MIT License.
-//
-
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
