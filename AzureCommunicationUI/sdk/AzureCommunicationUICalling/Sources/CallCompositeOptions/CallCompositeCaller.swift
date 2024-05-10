a314144d7f0a31029a706f12ed451c82913b39ce
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2024-05-09 18:46:20 -0700

    [Calling][Task] public api for 1 to 1 calling and CallKit (#862)

---
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallCompositeCaller.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+import AzureCommunicationCommon
+
+/// Call composite caller.
+public struct CallCompositeCaller {
+    /// Caller display name.
+    public let displayName: String
+
+    /// Caller CommunicationIdentifier.
+    public let identifier: CommunicationIdentifier
+
+    /// Create an instance of a CallCompositeCaller.
+    /// - Parameters:
+    ///   - displayName: Caller display name.
+    ///   - identifier: Caller CommunicationIdentifier.
+    public init(displayName: String,
+                identifier: CommunicationIdentifier) {
+        self.displayName = displayName
+        self.identifier = identifier
+    }
+}
--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallCompositeOptions.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallCompositeOptions.swift
@@ -15,6 +15,10 @@ public struct CallCompositeOptions {
     private(set) var setupScreenOrientation: OrientationOptions?
     private(set) var callingScreenOrientation: OrientationOptions?
     private(set) var callScreenOptions: CallScreenOptions?
+    private(set) var callKitOptions: CallCompositeCallKitOptions?
+    private(set) var disableInternalPushForIncomingCall = false
+    private(set) var displayName: String?
+
     /// Creates an instance of CallCompositeOptions with related options.
     /// - Parameter theme: ThemeOptions for changing color pattern.
     ///  Default value is `nil`.
@@ -28,13 +32,20 @@ public struct CallCompositeOptions {
     ///  while on the call. Default value is `false`.
     /// - Parameter enableSystemPictureInPictureWhenMultitasking: enables syspem Picture-in-Picture while
     ///  enableMultitasking is on and user navigates away from call view. Default value is `false`.
+    /// - Parameter callKitOptions: CallKitOptions for specifying CallKit customization. Default value is `nil`.
+    /// - Parameter disableInternalPushForIncomingCall: Disables internal push for incoming call. 
+    ///  default value is `false`.
+    /// - Parameter displayName: Display name for the user. Default value is `nil`.
     public init(theme: ThemeOptions? = nil,
                 localization: LocalizationOptions? = nil,
                 setupScreenOrientation: OrientationOptions? = nil,
                 callingScreenOrientation: OrientationOptions? = nil,
                 enableMultitasking: Bool = false,
                 enableSystemPictureInPictureWhenMultitasking: Bool = false,
-                callScreenOptions: CallScreenOptions? = nil) {
+                callScreenOptions: CallScreenOptions? = nil,
+                callKitOptions: CallCompositeCallKitOptions? = nil,
+                disableInternalPushForIncomingCall: Bool = false,
+                displayName: String? = nil) {
         self.themeOptions = theme
         self.localizationOptions = localization
         self.setupScreenOrientation = setupScreenOrientation
@@ -42,5 +53,8 @@ public struct CallCompositeOptions {
         self.enableMultitasking = enableMultitasking
         self.enableSystemPipWhenMultitasking = enableSystemPictureInPictureWhenMultitasking
         self.callScreenOptions = callScreenOptions
+        self.callKitOptions = callKitOptions
+        self.disableInternalPushForIncomingCall = disableInternalPushForIncomingCall
+        self.displayName = displayName
     }
 }
