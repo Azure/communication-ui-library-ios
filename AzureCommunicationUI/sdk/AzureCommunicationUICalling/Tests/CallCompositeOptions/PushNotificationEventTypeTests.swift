8d82ce57c90d64498e8fc7d937ab93b049007630
iaulakh <iaulakh@microsoft.com>
2024-06-11 11:43:57 -0700

    adding more tests

---
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/CallCompositeOptions/PushNotificationEventTypeTests.swift
@@ -0,0 +1,41 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+import XCTest
+import AzureCommunicationCalling
+@testable import AzureCommunicationUICalling
+
+class PushNotificationEventType: XCTestCase {
+    func test_pushNotificationEventType_initialization() {
+        let incomingCall = AzureCommunicationUICalling.PushNotificationEventType.incomingCall
+        let incomingGroupCall = AzureCommunicationUICalling.PushNotificationEventType.incomingGroupCall
+        let incomingPstnCall = AzureCommunicationUICalling.PushNotificationEventType.incomingPstnCall
+        let stopRinging = AzureCommunicationUICalling.PushNotificationEventType.stopRinging
+
+        XCTAssertEqual(incomingCall.requestString, "incomingcall")
+        XCTAssertEqual(incomingGroupCall.requestString, "incominggroupcall")
+        XCTAssertEqual(incomingPstnCall.requestString, "incomingpstncall")
+        XCTAssertEqual(stopRinging.requestString, "stopringing")
+    }
+
+    func test_pushNotificationEventType_conversion() {
+        let azureEventType = AzureCommunicationCalling.PushNotificationEventType.incomingCall
+        let eventType = azureEventType.toPushNotificationEventType()
+        XCTAssertEqual(eventType, .incomingCall)
+
+        let azureEventTypeGroupCall = AzureCommunicationCalling.PushNotificationEventType.incomingGroupCall
+        let eventTypeGroupCall = azureEventTypeGroupCall.toPushNotificationEventType()
+        XCTAssertEqual(eventTypeGroupCall, .incomingGroupCall)
+
+        let azureEventTypePstn = AzureCommunicationCalling.PushNotificationEventType.incomingGroupCall
+        let eventTypeGroupPstn = azureEventTypePstn.toPushNotificationEventType()
+        XCTAssertEqual(eventTypeGroupPstn, .incomingGroupCall)
+
+        let azureEventTypeStopRinging = AzureCommunicationCalling.PushNotificationEventType.incomingGroupCall
+        let eventTypeStopRinging = azureEventTypeStopRinging.toPushNotificationEventType()
+        XCTAssertEqual(eventTypeStopRinging, .incomingGroupCall)
+    }
+}
