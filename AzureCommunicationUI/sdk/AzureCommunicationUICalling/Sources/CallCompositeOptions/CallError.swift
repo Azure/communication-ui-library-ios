ce638a8f76950347830b76823f90d413302a6184
iaulakh <iaulakh@microsoft.com>
2024-06-10 21:37:20 -0700

    Fix comments

---
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallCompositeOptions/CallError.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import Foundation
+
+/// Errors for call actions.
+public enum CallError: String, Error {
+    /// Call is not in progress.
+    case callIsNotInProgress
+}
