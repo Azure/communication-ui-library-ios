527b086dc554f77f22e2b73d98caf6d640805972
Pavel Prystinka <pprystinka@microsoft.com>
2024-06-14 03:45:44 -0700

    Add test

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Redux/Reducer/ToastNotificationReducerTests.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Tests/Redux/Reducer/ToastNotificationReducerTests.swift
@@ -7,13 +7,22 @@ import XCTest
 @testable import AzureCommunicationUICalling
 
 class ToastNotificationReducerTests: XCTestCase {
-    func test_permissionReducer_reduce_when_audioPermissionSet_shouldReturnAudioPermissionGranted() {
+    func test_toastNotificationAction_reduce_when_cameraStartFailed_then_cameraStartFailed() {
         let state = ToastNotificationState()
         let action = ToastNotificationAction.showNotification(kind: .cameraStartFailed)
         let sut = makeSUT()
         let resultState = sut.reduce(state, action)
 
         XCTAssertEqual(resultState.status, .cameraStartFailed)
+    }
+
+    func test_toastNotificationAction_reduce_when_dismissNotification_then_nil() {
+        let state = ToastNotificationState()
+        let action = ToastNotificationAction.dismissNotification
+        let sut = makeSUT()
+        let resultState = sut.reduce(state, action)
+
+        XCTAssertNil(resultState.status)
     }
 }
 
