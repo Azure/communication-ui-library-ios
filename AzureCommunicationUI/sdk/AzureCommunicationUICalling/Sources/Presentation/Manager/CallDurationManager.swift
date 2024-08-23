8ef055e72fc6768ebb9286efd890862575a6bbb0
Mohtasim <mbellah@microsoft.com>
2024-08-22 17:49:22 -0700

    Pipeline raw log build error fix
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Manager/CallDurationManager.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/Manager/CallDurationManager.swift
@@ -26,31 +26,31 @@ class CallDurationManager: CallTimerAPI, ObservableObject {
 
     func onStart() {
         startTimer()
-        isStarted = true
+        self.isStarted = true
     }
 
     func onStop() {
-        timer?.invalidate()
-        timer = nil
-        isStarted = false
+        self.timer?.invalidate()
+        self.timer = nil
+        self.isStarted = false
         self.timeElapsed = 0
     }
 
     func onReset() {
-        timer?.invalidate()
+        self.timer?.invalidate()
         self.timeElapsed = 0
-        timerTickStateFlow = "00:00"
+        self.timerTickStateFlow = "00:00"
     }
 
     private func startTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
             guard let self = self else {
                 return
             }
             self.timeElapsed += 1
-            timerTickStateFlow = String(format: "%02d:%02d", Int(self.timeElapsed) / 60, Int(self.timeElapsed) % 60)
+            self.timerTickStateFlow = String(format: "%02d:%02d", Int(self.timeElapsed) / 60, Int(self.timeElapsed) % 60)
             if self.timeElapsed > 3600 {
-                timerTickStateFlow = String(format: "%02d:%02d:%02d",
+                self.timerTickStateFlow = String(format: "%02d:%02d:%02d",
                                             Int(self.timeElapsed) / 3600, Int(self.timeElapsed) / 60,
                                             Int(self.timeElapsed) % 60)
             }
