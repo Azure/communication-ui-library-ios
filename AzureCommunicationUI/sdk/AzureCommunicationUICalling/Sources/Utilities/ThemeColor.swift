7fddd3a5b93929de34d4373c9bb64d55deb0f59a
Nancy <177864805+inancyGit@users.noreply.github.com>
2024-09-26 18:14:43 -0400

    Changed ThemeColor to class
    Changed ThemeColor to class

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Utilities/ThemeColor.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Utilities/ThemeColor.swift
@@ -7,9 +7,7 @@ import Foundation
 import UIKit
 import FluentUI
 
-struct ThemeColor: ThemeOptions {
-    init() {
-    }
+class ThemeColor: ThemeOptions {
     var colorSchemeOverride: UIUserInterfaceStyle {
         return .unspecified
     }
