f3a22fb79c7ae1de94fb8c5f6c66cfd897025ce2
shaunasong <93549644+ShaunaSong@users.noreply.github.com>
2024-11-25 22:37:20 -0800

    Update title logic

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingViewComponent/CaptionsRtt/CaptionsAndRttInfoViewModel.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingViewComponent/CaptionsRtt/CaptionsAndRttInfoViewModel.swift
@@ -50,7 +50,7 @@ class CaptionsAndRttInfoViewModel: ObservableObject {
     }
 
     private func setupItems(state: AppState) {
-        if isRttDisplayed && isCaptionsDisplayed {
+        if isRttDisplayed && state.captionsState.isCaptionsOn {
             title = localizationProvider.getLocalizedString(.rttCaptionsListTitle)
         } else if isRttDisplayed {
             title = localizationProvider.getLocalizedString(.rttListTitle)
