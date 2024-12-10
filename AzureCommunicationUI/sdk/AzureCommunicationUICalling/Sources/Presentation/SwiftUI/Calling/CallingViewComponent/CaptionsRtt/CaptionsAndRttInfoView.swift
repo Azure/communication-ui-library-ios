3060bbcb02cbe2dc109f8546c5ae6e15fec7e1a1
shaunasong <93549644+ShaunaSong@users.noreply.github.com>
2024-12-09 16:51:22 -0800

    Update the scroll logic

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingViewComponent/CaptionsRtt/CaptionsAndRttInfoView.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/Presentation/SwiftUI/Calling/CallingViewComponent/CaptionsRtt/CaptionsAndRttInfoView.swift
@@ -9,73 +9,48 @@ struct CaptionsAndRttInfoView: View {
     @ObservedObject var viewModel: CaptionsAndRttInfoViewModel
     var avatarViewManager: AvatarViewManagerProtocol
     @State private var isLastItemVisible = true
-    @State private var previousDrawerHeight: CGFloat = 0 // Track the previous height
+    @State private var previousDrawerHeight: CGFloat = 0
     @Environment(\.verticalSizeClass) var verticalSizeClass
-
-    // Define a PreferenceKey to capture the height
-    struct HeightPreferenceKey: PreferenceKey {
-        static var defaultValue: CGFloat = 0
-
-        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-            value = max(value, nextValue())
-        }
-    }
-
-    // Debounce related state
-    @State private var scrollWorkItem: DispatchWorkItem?
 
     var body: some View {
         GeometryReader { geometry in
-            let parentFrame = geometry
             if viewModel.isLoading {
                 loadingView
             } else {
-                ScrollViewReader { scrollView in
-                    ScrollView {
-                        content(scrollView: scrollView, parentFrame: parentFrame)
-                            .frame(minHeight: geometry.size.height, alignment: .bottom) // Fix the height
-                            .frame(maxWidth: .infinity)
-                            .background(Color(StyleProvider.color.drawerColor))
-                            // Hidden GeometryReader to capture the height
-                            .background(
-                                GeometryReader { geo in
-                                    Color.clear
-                                        .preference(key: HeightPreferenceKey.self, value: geo.size.height)
-                                }
-                            )
-                    }
-                    // Listen for changes in displayData and drawer height
-                    .onPreferenceChange(HeightPreferenceKey.self) { newHeight in
-                        if newHeight != previousDrawerHeight {
-                            previousDrawerHeight = newHeight
+                contentView(geometry: geometry)
+            }
+        }
+    }
 
-                            // Cancel any existing scroll action
-                            scrollWorkItem?.cancel()
+    // MARK: - Subviews
 
-                            // Create a new work item with debounce delay
-                            let workItem = DispatchWorkItem {
-                                if isLastItemVisible {
-                                    scrollToBottom(scrollView)
-                                }
-                            }
+    @ViewBuilder
+    private func contentView(geometry: GeometryProxy) -> some View {
+        let containerHeight: CGFloat = geometry.size.height
 
-                            // Assign and execute the work item after a delay
-                            scrollWorkItem = workItem
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: workItem)
-                        }
-                    }
-                    .onChange(of: viewModel.displayData) { _ in
-                        if isLastItemVisible {
-                            scrollToBottom(scrollView)
-                        }
-                    }
+        ScrollViewReader { scrollView in
+            ScrollView {
+                contentListView(scrollView: scrollView, parentGeometry: geometry)
+                    .frame(minHeight: containerHeight, alignment: .bottom)
+                    .frame(maxWidth: .infinity)
+            }
+            .background(Color(StyleProvider.color.drawerColor))
+            .onChange(of: containerHeight) { newHeight in
+                if newHeight != previousDrawerHeight {
+                    previousDrawerHeight = newHeight
+                    scrollToBottom(scrollView)
+                }
+            }
+            .onChange(of: viewModel.displayData) { _ in
+                if isLastItemVisible {
+                    scrollToBottom(scrollView)
                 }
             }
         }
     }
 
     @ViewBuilder
-    private func content(scrollView: ScrollViewProxy, parentFrame: GeometryProxy) -> some View {
+    private func contentListView(scrollView: ScrollViewProxy, parentGeometry: GeometryProxy) -> some View {
         VStack(spacing: 0) {
             ForEach(viewModel.displayData.indices, id: \.self) { index in
                 let data = viewModel.displayData[index]
@@ -89,43 +64,27 @@ struct CaptionsAndRttInfoView: View {
                         localizationProvider: viewModel.localizationProvider
                     )
                     .id(viewModel.displayData[index].id)
-                    .background(lastItemBackground(index: index, parentFrame: parentFrame))
+                    .background(lastItemBackgroundIfNeeded(index: index, parentFrame: parentGeometry))
+                }
+            }.onAppear {
+                if isLastItemVisible {
+                    scrollToBottom(scrollView)
                 }
             }
         }
     }
 
     @ViewBuilder
-    private func lastItemBackground(index: Int, parentFrame: GeometryProxy) -> some View {
-        // Check if the last item is visible on the screen
+    private func lastItemBackgroundIfNeeded(index: Int, parentFrame: GeometryProxy) -> some View {
         if index == viewModel.displayData.indices.last {
             GeometryReader { geo in
                 Color.clear
                     .onAppear {
                         checkLastItemVisibility(geometry: geo, parentFrame: parentFrame)
                     }
-                    .onChange(of: viewModel.displayData) { _ in
-                        checkLastItemVisibility(geometry: geo, parentFrame: parentFrame)
-                    }
             }
-        }
-    }
-
-    private func scrollToBottom(_ scrollView: ScrollViewProxy) {
-        if let lastID = viewModel.displayData.last?.id {
-            withAnimation {
-                scrollView.scrollTo(lastID, anchor: .bottom)
-            }
-        }
-    }
-
-    private func checkLastItemVisibility(geometry: GeometryProxy, parentFrame: GeometryProxy) {
-        let itemFrame = geometry.frame(in: .global)
-        let parentBounds = parentFrame.frame(in: .global)
-
-        let isVisible = itemFrame.maxY > parentBounds.minY && itemFrame.minY < parentBounds.maxY
-        if isLastItemVisible != isVisible {
-            isLastItemVisible = isVisible
+        } else {
+            EmptyView()
         }
     }
 
@@ -160,5 +119,30 @@ struct CaptionsAndRttInfoView: View {
         .background(Color(StyleProvider.color.surface))
         .cornerRadius(8)
         .padding(.horizontal, 10)
+        .id("RTTInfoView")
+    }
+
+    // MARK: - Helper Methods
+
+    private func scrollToBottom(_ scrollView: ScrollViewProxy) {
+        if let lastID = viewModel.displayData.last?.id {
+            withAnimation {
+                scrollView.scrollTo(lastID, anchor: .bottom)
+            }
+        } else if viewModel.isRttDisplayed {
+            withAnimation {
+                scrollView.scrollTo("RTTInfoView", anchor: .bottom)
+            }
+        }
+    }
+
+    private func checkLastItemVisibility(geometry: GeometryProxy, parentFrame: GeometryProxy) {
+        let itemFrame = geometry.frame(in: .global)
+        let parentBounds = parentFrame.frame(in: .global)
+        let isVisible = itemFrame.maxY > parentBounds.minY && itemFrame.minY < parentBounds.maxY
+
+        if isLastItemVisible != isVisible {
+            isLastItemVisible = isVisible
+        }
     }
 }
